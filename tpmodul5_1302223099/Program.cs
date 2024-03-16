47cc8fb8abbf2c7edeec5f5f6d43b2df6711c0af
Muhammad Bintang Al-Fath <alfathbintangmuhammad@gmail.com>
2024-03-16 19:23:37 +0700

    menambahkan class DataGeneric

--- tpmodul5_1302223099/Program.cs
+++ tpmodul5_1302223099/Program.cs
@@ -1,7 +1,36 @@
 ﻿internal class Program
 {
+    public class HaloGeneric
+    {
+        public void SapaUser<T>(T x)
+        {
+            Console.WriteLine("Halo user " + x);
+        }
+    }
+
+    public class DataGeneric<T>
+    {
+        private T data;
+        public DataGeneric(T data) 
+        {
+            this.data = data;
+        }
+        public void PrintData()
+        {
+            Console.WriteLine("Data yang tersimpan adalah: " + data);
+        }
+    }
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        HaloGeneric halo = new HaloGeneric();
+        string nama;
+        Console.Write("Nama: ");
+        nama = Console.ReadLine();
+        halo.SapaUser(nama);
+
+        Console.Write("NIM: ");
+        string nim = Console.ReadLine();
+        DataGeneric<string> data = new DataGeneric<string>(nim); 
+        data.PrintData();
     }
 }
--- tpmodul5_1302223099/obj/Debug/net8.0/tpmodul5_1302223099.AssemblyInfo.cs
+++ tpmodul5_1302223099/obj/Debug/net8.0/tpmodul5_1302223099.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tpmodul5_1302223099")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f1cc1cc05f1929039d89f22eb6fb1b66d48d344")]
 [assembly: System.Reflection.AssemblyProductAttribute("tpmodul5_1302223099")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tpmodul5_1302223099")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
