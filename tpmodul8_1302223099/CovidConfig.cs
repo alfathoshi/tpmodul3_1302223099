68d28af2ce707c9971ad506684aef2ab2a746cc5
Muhammad Bintang Al-Fath <alfathbintangmuhammad@gmail.com>
2024-04-15 00:58:30 +0700

    menyelesaikan program dengan runtime configuration

---
+++ tpmodul8_1302223099/CovidConfig.cs
@@ -0,0 +1,66 @@
+﻿using System.Text.Json;
+
+namespace tpmodul8_1302223099
+{
+    public class CovidConfig
+    {
+        public class Konfig
+        {
+            public string pesan_ditolak { get; set; }
+            public string pesan_diterima { get; set; }
+            public string satuan_suhu { get; set; }
+            public double batas_hari_demam { get; set; }
+        }
+
+        public Konfig konfig;
+
+        public CovidConfig()
+        {
+            try
+            {
+                ReadKonfigFile();
+            }
+            catch
+            {
+                SetDefault();
+                WriteKonfigFile();
+            }
+        }
+
+        public void SetDefault()
+        {
+            konfig = new Konfig();
+            konfig.pesan_ditolak = "Anda tidak diperbolehkan masuk ke dalam gedung ini";
+            konfig.pesan_diterima = "Anda dipersilakan masuk ke dalam gedung ini";
+            konfig.batas_hari_demam = 14;
+            konfig.satuan_suhu = "celcius";
+        }
+
+        private const string filePath = "covid_config.json";
+
+        public void ReadKonfigFile()
+        {
+            String configJsonData = File.ReadAllText(filePath);
+            konfig = JsonSerializer.Deserialize<Konfig>(configJsonData);
+        }
+
+        public void WriteKonfigFile()
+        {
+            JsonSerializerOptions options = new JsonSerializerOptions()
+            {
+                WriteIndented = true
+            };
+
+            String json = JsonSerializer.Serialize(konfig);
+            File.WriteAllText(filePath, json);
+        }
+        
+        public void UbahSatuan()
+        {
+            konfig.satuan_suhu = konfig.satuan_suhu == "celcius" ? "fahrenheit" : "celcius";
+            WriteKonfigFile();
+        }
+
+    }
+}
+
--- tpmodul8_1302223099/Program.cs
+++ tpmodul8_1302223099/Program.cs
@@ -1,7 +1,29 @@
-﻿internal class Program
+﻿using tpmodul8_1302223099;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        CovidConfig config = new CovidConfig();
+
+        Console.WriteLine($"Berapa suhu badan anda saat ini? Dalam nilai {config.konfig.satuan_suhu}");
+        double suhu = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Berapa hari yang lalu (perkiraan) anda terakhir memiliki gejala demam?");
+        int hariDeman = int.Parse(Console.ReadLine());
+
+        if ((config.konfig.satuan_suhu == "celcius" && suhu >= 36.5 && suhu <= 37.5) ||
+            (config.konfig.satuan_suhu == "fahrenheit" && suhu >= 97.7 && suhu <= 99.5) &&
+            hariDeman < config.konfig.batas_hari_demam)
+        {
+            Console.WriteLine(config.konfig.pesan_diterima);
+        }
+        else
+        {
+            Console.WriteLine(config.konfig.pesan_ditolak);
+        }
+
+        config.UbahSatuan();
+        Console.WriteLine($"Selanjutnya, satuan diubah ke dalam {config.konfig.satuan_suhu}");
     }
 }
