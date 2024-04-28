c2cbb81aaff9971bde041b434b2f9bf794b501e3
Muhammad Bintang Al-Fath <alfathbintangmuhammad@gmail.com>
2024-04-28 17:29:16 +0700

    menambahkan API mahasiswa

---
+++ tpmodul10_1302223099/Controllers/MahasiswaController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace tpmodul10_1302223099.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MahasiswaController : ControllerBase
+    {
+
+        private static List<Mahasiswa> dataMahasiswa = new List<Mahasiswa>
+        {
+            new Mahasiswa("Muhammad Bintang Al-Fath", "130223099"),
+            new Mahasiswa("Wildan Syukri Ni'am", "130220005"),
+            new Mahasiswa("Arga Adolf Lumunon", "130223038"),
+            new Mahasiswa("Faris Azhar Dwiputra", "1302223123"),
+            new Mahasiswa("Farah Amalia Putri", "1302223137")
+        };
+
+        [HttpGet]
+        public IEnumerable<Mahasiswa> Get()
+        {
+            return dataMahasiswa;
+        }
+
+        [HttpPost]
+        public void Post([FromBody] Mahasiswa value)
+        {
+            dataMahasiswa.Add(value);
+        }
+
+        [HttpGet("{id}")]
+        public Mahasiswa Get(int id)
+        {
+            return dataMahasiswa[id];
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            dataMahasiswa.RemoveAt(id);
+        }
+
+    }    
+}
+
+
---
+++ tpmodul10_1302223099/Mahasiswa.cs
@@ -0,0 +1,14 @@
+﻿namespace tpmodul10_1302223099
+{
+    public class Mahasiswa
+    {
+        public string Nama { get; set; }
+        public string NIM { get; set; }
+
+        public Mahasiswa(string nama, string nim) 
+        { 
+            this.Nama = nama;
+            this.NIM = nim;
+        }
+    }
+}
